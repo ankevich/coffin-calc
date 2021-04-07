ae877e56adcd882f363ae06b2850dada52f7dd1f
Annie Bauer <overheads165@gmail.com>
2021-04-07 17:22:11 +0300

    added input & button

---
+++ html/script.js
@@ -0,0 +1,10 @@
+const window2 = () => {
+  
+    document.getElementById('window').hidden = true
+    document.getElementById('window2').hidden = false
+}
+
+const hideWindow2 = () => {
+    document.getElementById('window').hidden = false
+    document.getElementById('window2').hidden = true
+  }
