ff05ea39ce5ab33d440caaadf8f5d229c9c881dc
Annie Bauer <overheads165@gmail.com>
2021-04-09 21:49:27 +0300

    added animation

--- html/script.js
+++ html/script.js
@@ -1,15 +1,25 @@
 const showWindow2 = () => {
+    document.getElementById('window').classList.toggle("hide")
     document.getElementById('window').style.visibility = 'hidden'
+
+    document.getElementById('window2').classList.toggle("show")
     document.getElementById('window2').style.visibility = 'visible'
 }
 
 const showWindow3 = () => {
+    document.getElementById('window2').classList.toggle("hide")
     document.getElementById('window2').style.visibility = 'hidden'
+
+    document.getElementById('window3').classList.toggle("show")
     document.getElementById('window3').style.visibility = 'visible'
 }
 
 const exit = () => {
+    document.getElementById('window2').classList.toggle("hide")
     document.getElementById('window2').style.visibility = 'hidden'
+
+    document.getElementById('window3').classList.toggle("hide")
     document.getElementById('window3').style.visibility = 'hidden'
+
     document.getElementById('window').style.visibility = 'visible'
 }
