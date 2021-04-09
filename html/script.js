4928ad4bd0f714800548d4d622fd0decece801fa
Annie Bauer <overheads165@gmail.com>
2021-04-09 21:20:34 +0300

    +gvozdi

--- html/script.js
+++ html/script.js
@@ -1,10 +1,15 @@
 const showWindow2 = () => {
-    document.getElementById('window').style.visibility = "hidden"
-    document.getElementById('window2').style.visibility = "visible"
+    document.getElementById('window').style.visibility = 'hidden'
+    document.getElementById('window2').style.visibility = 'visible'
 }
 
-
 const showWindow3 = () => {
-    document.getElementById('window2').style.visibility = "hidden"
-    document.getElementById('window3').style.visibility = "visible"
+    document.getElementById('window2').style.visibility = 'hidden'
+    document.getElementById('window3').style.visibility = 'visible'
+}
+
+const exit = () => {
+    document.getElementById('window2').style.visibility = 'hidden'
+    document.getElementById('window3').style.visibility = 'hidden'
+    document.getElementById('window').style.visibility = 'visible'
 }
